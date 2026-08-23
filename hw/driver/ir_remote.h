d1206be788c44f0710a89e09adb7ce212e773ce9
LeeKiBeom1 <soul3233@naver.com>
2026-08-23 16:03:13 +0900

    핀맵 작성Ver 0.1

--- hw/driver/ir_remote.h
+++ hw/driver/ir_remote.h
@@ -9,10 +9,10 @@
 void ir_remote_init(void);
 void ir_remote_update(void);
 
-/* 새 명령이 있으면 true와 함께 전달하고, 없으면 false 반환 */
+// 새 명령이 있으면 true와 함께 전달하고, 없으면 false 반환
 bool ir_remote_take_command(drive_command_t *command);
 
-/* IR 디코더가 해석한 원시 버튼 코드를 전달 */
+// IR 디코더가 해석한 원시 버튼 코드를 전달 
 void ir_remote_on_raw_code(uint32_t raw_code);
 
 #endif
